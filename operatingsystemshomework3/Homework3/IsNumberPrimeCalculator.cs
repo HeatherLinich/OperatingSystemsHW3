d42310442c529c482e53369feb51824a5da511b8
Heather Mayer <hlinich@luc.edu>
2014-10-20 11:57:16 -0500

    Completed project for HW #3

---
+++ operatingsystemshomework3/Homework3/IsNumberPrimeCalculator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3 {
+    internal class IsNumberPrimeCalculator {
+        private readonly ICollection<long> _primeNumbers;
+        private readonly Queue<long> _numbersToCheck;
+
+        public IsNumberPrimeCalculator(ICollection<long> primeNumbers, Queue<long> numbersToCheck) {
+            _primeNumbers = primeNumbers;
+            _numbersToCheck = numbersToCheck;
+        }
+
+        public void CheckIfNumbersArePrime() {
+            while (true) {
+                //lock
+                long numberToCheck = -1;
+                lock (Calculator._lock)
+                {
+                    if (_numbersToCheck.Count != 0)
+                    {
+                        //Console.WriteLine(_numbersToCheck.Count);
+                        numberToCheck = _numbersToCheck.Dequeue();
+                    }
+                }
+                if (numberToCheck != -1)
+                {
+                    //unlock
+                    if (IsNumberPrime(numberToCheck))
+                    {
+                        //lock
+                        lock (Calculator._lock)
+                        {
+                            _primeNumbers.Add(numberToCheck);
+                        }
+                        //unlock
+                    }
+                }
+            }
+        }
+
+        private bool IsNumberPrime(long numberWeAreChecking) {
+            const long firstNumberToCheck = 3;
+
+            if (numberWeAreChecking % 2 == 0) {
+                return false;
+            }
+            var lastNumberToCheck = Math.Sqrt(numberWeAreChecking);
+            for (var currentDivisor = firstNumberToCheck; currentDivisor <= lastNumberToCheck; currentDivisor += 2) {
+                if (numberWeAreChecking % currentDivisor == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ operatingsystemshomework3/Homework3/ProgressMonitor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Homework3 {
+    internal class ProgressMonitor {
+        private readonly List<long> _results;
+        public long TotalCount = 0;
+
+        public ProgressMonitor(List<long> results) {
+            _results = results;
+        }
+
+        public void Run() {
+            while (true) {
+                Thread.Sleep(100); // wait for 1/10th of a second
+
+                long currentcount;
+                //lock
+                lock (Calculator._lock)
+                {
+                    currentcount = _results.Count;
+                    TotalCount += currentcount;
+
+                    _results.Clear(); // clear out the current primes to save some memory
+                }
+
+                //unlock
+
+                if (currentcount > 0) {
+                    Console.WriteLine("{0} primes found so far", TotalCount);
+                }
+            }
+        }
+    }
+}
